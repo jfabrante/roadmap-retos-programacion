925143565f04852f41fa6d4ef96fd68ee95d6174
jabrante <mantrestel88@gmail.com>
2024-04-28 11:56:16 +0200

    Está terminado

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/jfabrante.py
@@ -0,0 +1,34 @@
+# https://www.python.org/
+
+# Comentario en una línea
+
+'''
+Esto es un comentario en 
+varias líneas
+'''
+
+"""
+Con comillas dobles también
+se puede comentar
+en varias líneas
+"""
+my_variable = "Mi variable"
+my_variable = "Nuevo valor variable"
+
+MY_CONSTANTE = "Mi constante" # por convención
+
+my_int = 1
+my_float = 1.5
+my_bool = True
+my_bool = False
+my_string = "Mi cadena de texto"
+my_other_string = "Mi otra cadena de texto"
+
+print("¡Hola, Python!")
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
+
+
